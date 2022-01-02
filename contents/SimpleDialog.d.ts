8ccfccb5a807c0774986f608fac95a076ec90097
happy-cola <77879738+happy-cola@users.noreply.github.com>
2022-01-02 16:49:47 +0800

    Update SimpleDialog.d.ts

--- contents/SimpleDialog.d.ts
+++ contents/SimpleDialog.d.ts
@@ -1,4 +1,6 @@
 /**
  * 显示简单的对话框
  */
-declare function showSimpleDialog(entity: Box3PlayerEntity,title?:string,content?:string):void
+declare function textDialog(entity: Box3PlayerEntity, title?: string, content?: string): Promise<string | Box3DialogSelectResponse>
+declare function selectDialog(entity: Box3PlayerEntity, title?: string, content?: string, options: Array<string>): Promise<string | Box3DialogSelectResponse>
+declare function inputDialog(entity: Box3PlayerEntity, title?: string, content?: string, confirmText?: string, placeholder?: string): Promise<string | Box3DialogSelectResponse>
